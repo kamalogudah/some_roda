e2e2ec37622b4867acd88679d65cd6abe02a913f
kamalogudah <mcpaul2058@gmail.com>
2025-06-06 15:31:56 +0300

    add on matcher

--- app.rb
+++ app.rb
@@ -6,11 +6,16 @@ class App < Roda
   mystery_guest = Pizza.new('Mozzarella')
 
   route do |r|
+    p r
     r.get 'hello', String do |name|
       "<h1>Hello #{name}!</h1>"
     end
     r.get 'mystery_guest' do
       "The Mystery Gest is: #{h mystery_guest}"
+    end
+
+    r.on 'time' do
+      "#{Time.now}"
     end
   end
 end

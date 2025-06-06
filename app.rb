ec4978327dcbafc0ef1d17818cc5860a7b91cd63
kamalogudah <mcpaul2058@gmail.com>
2025-06-06 16:56:33 +0300

    r.on and r.is

--- app.rb
+++ app.rb
@@ -34,5 +34,23 @@ class App < Roda
     r.on 'goodbye' do
       "Goodbye, #{title}!"
     end
+
+    r.on 'posts' do
+      post_list = {
+        1 => 'Post[1]',
+        2 => 'Post[2]',
+        3 => 'Post[3]',
+        4 => 'Post[4]',
+        5 => 'Post[5]'
+      }
+
+      r.is Integer do |id|
+        post_list[id]
+      end
+
+      r.is do
+        post_list.values.map { |post| post }.join(' | ')
+      end
+    end
   end
 end

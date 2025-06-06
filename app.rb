a1271f8b33d491d69e37b5c7a8ab13e6a2c6ac8e
kamalogudah <mcpaul2058@gmail.com>
2025-06-06 15:16:31 +0300

    added plugin h

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'roda'
+Pizza = Struct.new(:flavor)
 
 class App < Roda
+  plugin :h
+  mystery_guest = Pizza.new('Mozzarella')
+
   route do |r|
     r.get 'hello', String do |name|
       "<h1>Hello #{name}!</h1>"
+    end
+    r.get 'mystery_guest' do
+      "The Mystery Gest is: #{h mystery_guest}"
     end
   end
 end

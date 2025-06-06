c02a5d3c907ce3a0f84f38390d56995382eda8d5
kamalogudah <mcpaul2058@gmail.com>
2025-06-06 16:31:37 +0300

    Basic routing

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ class App < Roda
 
   route do |r|
     p r
+    p 'ROUTE block'
     r.get 'hello', String do |name|
       "<h1>Hello #{name}!</h1>"
     end
@@ -16,6 +17,22 @@ class App < Roda
 
     r.on 'time' do
       "#{Time.now}"
+    end
+    r.on 'nothing' do
+      false
+    end
+
+    r.on 'integer' do
+      1
+    end
+    title = 'Sir'
+
+    r.on 'greet' do
+      "Hello, #{title}!"
+    end
+
+    r.on 'goodbye' do
+      "Goodbye, #{title}!"
     end
   end
 end
